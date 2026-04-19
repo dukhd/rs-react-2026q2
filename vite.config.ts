45059154b839172e3ed599cee4bec02a7a2fc313
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-04-19 23:01:59 +0100

    chore: scaffold project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  build: {
+    minify: true,
+    target: 'esnext',
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tailwindcss(), react()],
+});
