77b1a305718db41b62f6f714bda0af0519a72546
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-04-21 15:43:28 +0100

    feat: add reusable button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,28 @@
+import { PureComponent } from 'react';
+
+interface ButtonProps {
+  text: string;
+  type: 'button' | 'submit' | 'reset';
+  onClick: () => void;
+  className?: string;
+}
+
+class Button extends PureComponent<ButtonProps> {
+  render() {
+    const { text, type, onClick, className } = this.props;
+
+    const baseClassName = 'cursor-pointer';
+    const hoverClassName = 'hover:font-bold';
+
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        className={`${baseClassName} ${hoverClassName} ${className}`}
+      >
+        {text}
+      </button>
+    );
+  }
+}
+export default Button;
