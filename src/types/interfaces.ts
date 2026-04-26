5b08ec7f7a8d045530df656c5103efc526b11c90
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-04-26 18:15:19 +0100

    feat: add response interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -4,3 +4,33 @@ export interface CardData {
   title: string;
   description: string;
 }
+
+interface CharacterLocation {
+  name: string;
+  url: string;
+}
+
+export interface CharacterSchemaResponse {
+  id: number;
+  name: string;
+  status: 'Alive' | 'Dead' | 'unknown';
+  species: string;
+  type: string;
+  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
+  origin: CharacterLocation;
+  location: CharacterLocation;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface AllCharactersSchema {
+  info: {
+    count: number;
+    pages: number;
+    next: null | string;
+    prev: null | string;
+  };
+  results: CharacterSchemaResponse[];
+}
