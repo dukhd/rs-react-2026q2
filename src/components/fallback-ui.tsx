f01fca7f875130e353c8a48efc679e9d8a823a93
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-01 23:11:02 +0100

    feat: update fallback ui with image

--- src/components/fallback-ui.tsx
+++ src/components/fallback-ui.tsx
@@ -2,6 +2,11 @@ import { PureComponent } from 'react';
 
 import Button from './ui/button';
 
+const FALLBACK_CONTENT = {
+  TITLE: 'Oooooops! Something went wrong!',
+  IMG_ALT: 'The main characters of Rick and Morty',
+};
+
 interface Props {
   onReturn: () => void;
 }
@@ -10,7 +15,11 @@ class FallbackUI extends PureComponent<Props> {
     const { onReturn } = this.props;
     return (
       <div className="flex flex-col items-center gap-2">
-        <h1>Oooooops! Something went wrong!</h1>
+        <h1>{FALLBACK_CONTENT.TITLE}</h1>
+        <img
+          src="/src/assets/images/rick-and-morty-30973.webp"
+          alt={FALLBACK_CONTENT.IMG_ALT}
+        />
         <Button text="Return back" type="button" onClick={onReturn} />
       </div>
     );
