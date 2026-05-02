c15c4c92f2959f15deb62d7802d687d1c73677e0
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-02 23:40:44 +0100

    feat: update main title and improve its styling

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,11 @@ class App extends Component {
     return (
       <main className="flex flex-col gap-6">
         <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-center sm:justify-between">
-          <h1 className="text-accent text-shadow-custom text-center text-3xl font-bold sm:text-left sm:text-4xl md:text-5xl">
-            Rick and Morty
+          <h1 className="text-accent text-shadow-custom-small sm:text-shadow-custom text-center text-3xl font-bold sm:text-left sm:text-4xl md:text-5xl">
+            <span>Rick and Morty </span>
+            <span className="text-shadow-custom-small text-xl sm:text-2xl">
+              Explorer
+            </span>
           </h1>
           <ErrorButton />
         </div>
