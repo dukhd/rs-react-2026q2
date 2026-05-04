63194d99fd14609b7760d43be2d9ad4e07f4e528
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-04 20:51:43 +0100

    test: add unit tests for loader component

---
+++ src/components/loader/loader.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import Loader from './loader';
+
+describe('Loader', () => {
+  test('Should render loader with "Loading..." text', () => {
+    render(<Loader />);
+    const element = screen.getByRole('status');
+    expect(element).toHaveTextContent(/Loading\.\.\./i);
+  });
+
+  test('Should contain a screen-reader only text', () => {
+    render(<Loader />);
+    const srOnlyText = screen.getByText(/loading\.\.\./i, {
+      selector: '.sr-only',
+    });
+    expect(srOnlyText).toBeInTheDocument();
+  });
+});
