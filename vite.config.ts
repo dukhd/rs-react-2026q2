245ed96deb247acda3163bd93d9d6f8e0df2709f
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-04 20:04:34 +0100

    chore: setup testing environment with vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -16,4 +17,31 @@ export default defineConfig({
     tsconfigPaths: true,
   },
   plugins: [tailwindcss(), react()],
+  test: {
+    exclude: ['node_modules', 'dist'],
+    ui: true,
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/__tests__/setup-tests.ts',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/main.tsx',
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/__tests__/*',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
 });
