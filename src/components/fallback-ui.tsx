f13d9dbca966e5b5222442523f440f3afaf0ad67
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-11 00:06:21 +0100

    refactor: convert FallbackUI to functional component

--- src/components/fallback-ui.tsx
+++ src/components/fallback-ui.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import type { JSX } from 'react';
 
 import fallbackImage from '@/assets/images/rick-and-morty-30973.webp';
 
@@ -12,19 +12,17 @@ const FALLBACK_CONTENT = {
 interface Props {
   onReturn: () => void;
 }
-class FallbackUI extends PureComponent<Props> {
-  render() {
-    const { onReturn } = this.props;
-    return (
-      <div className="mt-20 flex flex-col items-center gap-2">
-        <h1 className="text-accent text-center text-3xl font-medium tracking-wide">
-          {FALLBACK_CONTENT.TITLE}
-        </h1>
-        <img src={fallbackImage} alt={FALLBACK_CONTENT.IMG_ALT} width={350} />
-        <Button text="Try again" type="button" onClick={onReturn} />
-      </div>
-    );
-  }
-}
+
+const FallbackUI = ({ onReturn }: Props): JSX.Element => {
+  return (
+    <div className="mt-20 flex flex-col items-center gap-2">
+      <h1 className="text-accent text-center text-3xl font-medium tracking-wide">
+        {FALLBACK_CONTENT.TITLE}
+      </h1>
+      <img src={fallbackImage} alt={FALLBACK_CONTENT.IMG_ALT} width={350} />
+      <Button text="Try again" type="button" onClick={onReturn} />
+    </div>
+  );
+};
 
 export default FallbackUI;
