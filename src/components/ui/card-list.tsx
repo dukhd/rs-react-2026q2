a174d77c2c2714704053119101ba0487e5d22f13
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-10 23:55:52 +0100

    refactor: convert CardList to functional component

--- src/components/ui/card-list.tsx
+++ src/components/ui/card-list.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import type { JSX } from 'react';
 
 import type { CharacterSchema } from '@/types/interfaces';
 
@@ -8,25 +8,21 @@ interface CardListProps {
   cards: CharacterSchema[];
 }
 
-class CardList extends PureComponent<CardListProps> {
-  render() {
-    const { cards } = this.props;
-
-    return (
-      <ul className="mx-auto grid max-w-6xl grid-cols-1 gap-x-8 gap-y-6 lg:grid-cols-2">
-        {cards.map((card, index) => {
-          return (
-            <li
-              key={card.id}
-              className="w-full max-w-125 list-none justify-self-center"
-            >
-              <Card card={card} priority={index < 2} />
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+const CardList = ({ cards }: CardListProps): JSX.Element => {
+  return (
+    <ul className="mx-auto grid max-w-6xl grid-cols-1 gap-x-8 gap-y-6 lg:grid-cols-2">
+      {cards.map((card, index) => {
+        return (
+          <li
+            key={card.id}
+            className="w-full max-w-125 list-none justify-self-center"
+          >
+            <Card card={card} priority={index < 2} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default CardList;
