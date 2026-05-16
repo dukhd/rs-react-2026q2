1d9ff570c23be54362e2bc4519f65e4530d6650a
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-16 17:41:16 +0100

    test: move tests from app component to home page

--- src/pages/home-page.test.tsx
+++ src/pages/home-page.test.tsx
@@ -1,20 +1,29 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router';
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
 import { errorHandlers } from '@/__tests__/msw/error-handlers';
 import { server } from '@/__tests__/msw/server';
+import { resolveLoading } from '@/__tests__/utils/resolve-loading';
 import { STORAGE_KEYS } from '@/constants/storage-keys';
 
-import { resolveLoading } from './__tests__/utils/resolve-loading';
-import App from './App';
+import HomePage from './home-page';
 
-describe('App Component', () => {
+const renderHomePage = () => {
+  return render(
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  );
+};
+
+describe('Home Page Component', () => {
   beforeEach(() => {
     vi.useFakeTimers({ shouldAdvanceTime: true });
     localStorage.clear();
-    vi.spyOn(localStorage, 'setItem');
-    vi.spyOn(localStorage, 'getItem');
+    vi.spyOn(Storage.prototype, 'setItem');
+    vi.spyOn(Storage.prototype, 'getItem');
   });
 
   afterEach(() => {
@@ -24,22 +33,23 @@ describe('App Component', () => {
   });
 
   test('Should initialize with search term from localStorage', () => {
-    const getItemSpy = vi.spyOn(Storage.prototype, 'getItem');
     const savedTerm = 'Rick';
     localStorage.setItem(STORAGE_KEYS.SEARCH_TERM, savedTerm);
 
-    render(<App />);
+    renderHomePage();
 
     const input = screen.getByRole<HTMLInputElement>('searchbox');
     expect(input.value).toBe(savedTerm);
-    expect(getItemSpy).toHaveBeenCalledWith(STORAGE_KEYS.SEARCH_TERM);
+    expect(Storage.prototype.getItem).toHaveBeenCalledWith(
+      STORAGE_KEYS.SEARCH_TERM
+    );
   });
 
   test('Should perform initial API call on mount if search term exists in localStorage', async () => {
     const savedTerm = 'Rick';
     localStorage.setItem(STORAGE_KEYS.SEARCH_TERM, savedTerm);
 
-    render(<App />);
+    renderHomePage();
     expect(screen.getByRole('status')).toBeInTheDocument();
 
     await resolveLoading();
@@ -51,7 +61,7 @@ describe('App Component', () => {
 
   test('Should initialize with empty string if localStorage contains an empty string', () => {
     localStorage.setItem(STORAGE_KEYS.SEARCH_TERM, '');
-    render(<App />);
+    renderHomePage();
     const input = screen.getByRole<HTMLInputElement>('searchbox');
     expect(input.value).toBe('');
   });
@@ -59,8 +69,7 @@ describe('App Component', () => {
   test('Should update SearchResult when a new search is performed', async () => {
     const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
     const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
-
-    render(<App />);
+    renderHomePage();
 
     const input = screen.getByRole('searchbox');
     const button = screen.getByRole('button', { name: /search/i });
@@ -80,7 +89,7 @@ describe('App Component', () => {
   test('Should trim whitespace from search input before saving to localStorage', async () => {
     const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
     const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
-    render(<App />);
+    renderHomePage();
 
     const input = screen.getByRole('searchbox');
     const button = screen.getByRole('button', { name: /search/i });
@@ -88,6 +97,7 @@ describe('App Component', () => {
     await user.clear(input);
     await user.type(input, '  Morty  ');
     await user.click(button);
+
     await waitFor(() => {
       expect(setItemSpy).toHaveBeenCalledWith(
         STORAGE_KEYS.SEARCH_TERM,
@@ -101,7 +111,7 @@ describe('App Component', () => {
     const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
     server.use(errorHandlers.internalError());
 
-    render(<App />);
+    renderHomePage();
     await resolveLoading();
     const input = screen.getByRole('searchbox');
     const button = screen.getByRole('button', { name: /search/i });
@@ -117,7 +127,7 @@ describe('App Component', () => {
   });
 
   test('Should render ErrorButton and it is present in the document', () => {
-    render(<App />);
+    renderHomePage();
     const errorButton = screen.getByRole('button', { name: /trigger error/i });
     expect(errorButton).toBeInTheDocument();
   });
