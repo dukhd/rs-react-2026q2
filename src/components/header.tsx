bcb1da049c00a56a3495d21757fa2b8b3980cdda
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-16 18:02:59 +0100

    refactor: update text style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router';
 const Header = (): JSX.Element => {
   return (
     <>
-      <div className="text-main flex w-full flex-col items-center gap-4 sm:flex-row sm:justify-between sm:gap-0">
+      <div className="flex w-full flex-col items-center gap-4 sm:flex-row sm:justify-between sm:gap-0">
         <h1 className="text-accent text-shadow-custom-small sm:text-shadow-custom flex items-end justify-center gap-1 text-center text-3xl font-bold sm:flex-row sm:justify-start sm:gap-2 sm:text-left sm:text-4xl">
           <span>Rick and Morty </span>
           <span className="text-main text-xl">Explorer</span>
         </h1>
-        <div className="flex gap-4 text-lg font-bold">
+        <div className="text-main flex gap-4 text-lg font-bold">
           <NavLink
             to="/"
             className={({ isActive }) =>
