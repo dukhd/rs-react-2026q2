572ae167efa417b66f316c482f7dd3b62313be79
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-05-25 12:54:12 +0100

    feat: implement csv download

--- src/components/flayout.tsx
+++ src/components/flayout.tsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { unselectAll } from '@/store/selected-cards-slice';
 import type { AppDispatch, RootState } from '@/store/store';
+import { downloadCharactersCSV } from '@/utils/download-csv';
 
 import Button from './ui/button';
 
@@ -16,6 +17,10 @@ const Flayout = () => {
     dispatch(unselectAll());
   };
 
+  const handleDownload = () => {
+    downloadCharactersCSV(selectedCards);
+  };
+
   return (
     <div className="flex flex-col items-center gap-4 sm:flex-row sm:gap-8">
       <span className="min-w-30 text-xl font-semibold uppercase tabular-nums">
@@ -25,6 +30,7 @@ const Flayout = () => {
         <Button
           type="button"
           text="Download"
+          onClick={handleDownload}
           customClassName="bg-accent text-second px-6 py-1 text-sm sm:text-base"
         />
         <Button
